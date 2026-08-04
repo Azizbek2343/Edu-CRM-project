e730d19923770f5d914ec6dcc80b4f39ff496dfe
Azizbek2343 <nasritdinovAzizbek001@gmail.com>
2026-08-04 13:06:10 +0500

    GroupAtendance changing

--- src/pages/groups/groupAttendance/GroupAttendance.jsx
+++ src/pages/groups/groupAttendance/GroupAttendance.jsx
@@ -2,9 +2,43 @@ import { useParams } from "react-router-dom";
 import styles from "./GroupAttendance.module.css";
 
 const GroupAttendance = () => {
+
+  const students = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
+  const lessons = [
+    {
+      id: 1,
+      date: "1",
+    },
+    {
+      id: 2,
+      date: "3",
+    },
+    {
+      id: 3,
+      date: "5",
+    },
+    {
+      id: 4,
+      date: "8",
+    },
+  ];
   return (
     <div>
-        <h1>Group Attendance</h1>
+        <div className={styles.wrapper}>
+          <div className={styles.container}>
+            {lessons.map((lesson) => {
+              return (
+              <div key={lesson.id} className={styles.lesson}>
+                {students.map((student, i) => {
+                  return (
+                    <div key={i} className={styles.checking}>✅</div>
+                  )
+                })}
+              </div>
+              )
+            })}
+          </div>
+        </div>
     </div>
   )
 }
